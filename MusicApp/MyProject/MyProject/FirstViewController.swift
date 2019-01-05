101807bfde34f67c35fa8e3118cafd01f8bf8ae2
nicksl1986 <34469459+nicksl1986@users.noreply.github.com>
2019-01-05 12:25:19 +0200

    Add files via upload

---
+++ MusicApp/MyProject/MyProject/FirstViewController.swift
@@ -0,0 +1,127 @@
+//
+//  FirstViewController.swift
+//  MyProject
+//
+//  Created by Nick Slobodsky on 03/10/2018.
+//  Copyright © 2018 Nick Slobodsky. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+var songs : [String] = []
+
+var audioPlayer = AVAudioPlayer()
+
+var songSelected = false
+
+var timer = Timer()
+
+var currentTime = 0
+
+var songLength = 0
+
+var minutes = 0
+
+var seconds = 0
+
+var currentTrackIndex = 0
+
+var previousTrackIndex = 0
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+         gettingSongName()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        do
+        {
+            let audioPath = Bundle.main.path(forResource: songs[indexPath.row], ofType: ".mp3")
+            
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            
+            audioPlayer.play()
+            
+            currentTrackIndex = indexPath.row
+            
+            previousTrackIndex -= 1
+            
+            if previousTrackIndex < 0
+            {
+                previousTrackIndex = songs.count - 1
+            }
+            
+           songSelected = true
+            
+        }
+        catch
+        {
+            fatalError()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = songs[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    func gettingSongName()
+    {
+        let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        
+        do
+        {
+            let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            for song in songPath
+            {
+                var mySong = song.absoluteString
+                
+                if mySong.contains(".mp3")
+                {
+                    let findString = mySong.components(separatedBy: "/")
+                    
+                    mySong = findString[findString.count - 1]
+                    
+                    mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                    
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                    
+                    songs.append(mySong)
+                    
+                }
+            }
+            
+            myTableView.reloadData()
+        }
+        catch
+        {
+            fatalError()
+        }
+
+
+}
+
+}
+
---
+++ MusicApp/MyProject/MyProject/SecondViewController.swift
@@ -0,0 +1,188 @@
+//
+//  SecondViewController.swift
+//  MyProject
+//
+//  Created by Nick Slobodsky on 03/10/2018.
+//  Copyright © 2018 Nick Slobodsky. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SecondViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+       if songSelected == true
+       {
+          label.text = songs[currentTrackIndex]
+        
+          myImageView.loadGif(name: "music")
+        
+          timerRunning()
+        
+          slider.maximumValue = Float(Int(audioPlayer.duration))
+       }
+        else
+       {
+            myImageView.loadGif(name: "music")
+        
+            currentTrackIndex = 0
+        
+            previousTrackIndex = songs.count - 1
+        
+            nextSong()
+       }
+    
+    }
+    
+    
+    func nextSong()
+    {
+        let audioPath = Bundle.main.path(forResource: songs[currentTrackIndex], ofType: ".mp3")
+       
+        do
+        {
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+        }
+        catch
+        {
+            fatalError()
+        }
+        
+        audioPlayer.play()
+        
+        slider.maximumValue = Float(audioPlayer.duration)
+        
+        label.text = songs[currentTrackIndex]
+        
+        timerRunning()
+      
+    }
+   
+    func timeString(totalTime : Int) -> String
+    {
+        let minutes = (totalTime / 60) % 60
+        
+        let seconds = totalTime % 60
+        
+        return String(format: "%02d : %02d", minutes, seconds)
+    }
+    
+    @IBAction func play(_ sender: UIButton) {
+        
+        audioPlayer.play()
+        
+        startTimer()
+    }
+    
+    func startTimer()
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerRunning), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pause(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        audioPlayer.pause()
+        
+    }
+    
+    
+    @IBAction func previous(_ sender: UIButton) {
+        
+       timer.invalidate()
+        
+       slider.value = 0
+        
+       currentTrackIndex = previousTrackIndex
+        
+       previousTrackIndex -= 1
+        
+       if previousTrackIndex < 0
+       {
+            previousTrackIndex = songs.count - 1
+       }
+        
+        nextSong()
+        
+    }
+    
+    
+    @IBAction func next(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        previousTrackIndex = currentTrackIndex
+        
+        currentTrackIndex += 1
+        
+        if currentTrackIndex > songs.count - 1
+        {
+            
+            currentTrackIndex = 0
+        }
+        
+        nextSong()
+    }
+    
+    
+    @IBAction func slider(_ sender: UISlider) {
+        
+        audioPlayer.currentTime = TimeInterval(slider.value)
+    }
+    
+    
+    @objc func timerRunning()
+    {
+        if audioPlayer.isPlaying == true
+        {
+            currentTime = Int(audioPlayer.currentTime)
+            
+            slider.value = Float(currentTime)
+            
+            timeLabel.text = timeString(totalTime: currentTime)
+        }
+        else
+        {
+            timer.invalidate()
+            
+            timeLabel.text = timeString(totalTime: 0)
+            
+            slider.value = 0
+            
+            previousTrackIndex = currentTrackIndex
+            
+            currentTrackIndex += 1
+            
+            if currentTrackIndex > songs.count - 1
+            {
+                currentTrackIndex = 0
+            }
+            
+            nextSong()
+        }
+       
+    }
+    
+    
+
+}
+
+
+
+
